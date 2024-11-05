7dec64e12c7b10567d41c12f61fa9ff41b5104d1
Shishir Paudel <sishirpaudel7@gmail.com>
2024-11-05 16:36:30 -0500

    redesign dashboard page ui

--- RCEM/rcem_nextjs/app/admin/dashboard/page.js
+++ RCEM/rcem_nextjs/app/admin/dashboard/page.js
@@ -3,118 +3,115 @@ import PageLayout from "@/app/components/pageLayout";
 function page() {
   return (
     <PageLayout>
-      <div className="  flex flex-1 flex-col">
-        <div className="h-full w-full flex flex-col">
-          <div className="flex flex-row h-full">
-            <section className="basis-2/5 h-full text-black bg-white border border-solid border-black">
-              Section 1
-            </section>
-            <section className="basis-3/5 h-full bg-white flex flex-col">
-              <aside className="basis-2/5 w-full text-black bg-white border border-solid border-black px-6 py-3">
-                <form action="" method="post">
-                  <fieldset className="w-2/5 flex flex-col">
-                    <legend className="text-2x1 mb-2">Add Equipment</legend>
-                    <label
-                      htmlFor="equipmentName"
-                      className="flex-1 text-sm font-medium text-gray-700 space-y-6"
-                    >
+      <div className="w-full flex p-8">
+        <div className="flex gap-2 justify-center  w-full">
+          <div className="w-full p-2 flex flex-col gap-6">
+            <div className="overflow-y-auto h-[70vh]">
+              <div className="p-4 flex flex-col gap-2 ">
+                <h2 className="font-semibold text-lg">Available Equipments</h2>
+                <div className="flex gap-2">
+                  <input
+                    className="border px-4 py-2 rounded-lg w-full"
+                    placeholder="Search Equipments "
+                  />
+                  <div className="flex gap-2">
+                    <button className="px-4 py-2  border rounded-lg hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s">
+                      Search
+                    </button>
+                    <button className="px-4 py-2  border rounded-lg hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s">
+                      Refresh
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <table className="table table-xs w-full">
+                <thead className="bg-gray-200 rounded-lg">
+                  <tr>
+                    <th className="w-1/4 px-4 py-2 text-left">
                       Equipment Name
-                    </label>
-                    <input
-                      type="text"
-                      name="eqipmentName"
-                      id="equpmentName"
-                      className="flex-1 mt-1 mb-2 p-1 w-full text-sm border border-gray-600 shadow-sm"
-                      placeholder="Enter Equipment Name"
-                    />
-                    <label
-                      htmlFor="equipmentId"
-                      className="flex-1 text-sm font-medium text-gray-700 space-y-6"
-                    >
-                      Equipment ID
-                    </label>
-                    <input
-                      type="text"
-                      name="eqipmentId"
-                      id="equpmentId"
-                      className="flex-1 mt-1 mb-2 p-1 w-full text-sm border border-gray-600 shadow-sm"
-                      placeholder="Enter Equipment Id"
-                    />
-                    <label
-                      htmlFor="quantity"
-                      className="flex-1 text-sm font-medium text-gray-700 space-y-6"
-                    >
-                      Quantity
-                    </label>
-                    <input
-                      type="text"
-                      name="quantity"
-                      id="quantity"
-                      className="flex-1 mt-1 mb-2 p-1 w-full text-sm border border-gray-600 shadow-sm"
-                      placeholder="Enter Quantity"
-                    />
-                    <div className="flex flex-row flex-1 gap-x-2 justify-center">
-                      <button
-                        type="submit"
-                        className="p-1 text-sm border border-gray-600 shadow-sm"
-                      >
-                        Add Item
-                      </button>
-                      <button
-                        type="reset"
-                        className="p-1 text-sm border border-gray-600 shadow-sm"
-                      >
-                        Reset
-                      </button>
-                    </div>
-                  </fieldset>
-                </form>
-              </aside>
-              <aside className="flex flex-col basis-3/5 w-full text-black bg-white border border-solid border-black px-6 py-3 gap-y-3">
-                <nav className="flex flex-row w-1/4 border border-solid border-black gap-x-2">
-                  <button className="text-black text-sm p-1">
-                    All Bookings
-                  </button>
-                  <button className="text-black text-sm p-1">
-                    Pending Bookings
-                  </button>
-                </nav>
-                <table className="min-w-full border-collapse border border-gray-900">
-                  <thead>
-                    <tr>
-                      <th className="font-normal text-sm border border-gray-900 text-left px-2 py-1">
-                        Equipment Name
-                      </th>
-                      <th className="font-normal text-sm border border-gray-900 text-left px-2 py-1">
-                        Booked By
-                      </th>
-                      <th className="font-normal text-sm border border-gray-900 text-left px-2 py-1">
-                        Equipment ID
-                      </th>
-                      <th className="font-normal text-sm border border-gray-900 text-left px-2 py-1">
-                        Booked Time
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className="text-sm border border-gray-900 px-2 py-1">
-                        equipment1
-                      </td>
-                      <td className="text-sm border border-gray-900 px-2 py-1">
-                        booked1
-                      </td>
-                      <td className="text-sm border border-gray-900 px-2 py-1">
-                        id1
-                      </td>
-                      <td className="text-sm border border-gray-900 px-2 py-1">
-                        time1
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </aside>
-            </section>
+                    </th>
+                    <th className="w-1/4 px-4 py-2 text-left">Equipment ID</th>
+                    <th className="w-1/4 px-4 py-2 text-left">Quantity</th>
+                    <th className="w-1/4 px-4 py-2 text-left">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="w-1/4 px-4 py-2">Bicycles</td>
+                    <td className="w-1/4 px-4 py-2">54987</td>
+                    <td className="w-1/4 px-4 py-2">20</td>
+                    <td className="w-1/4 px-4 py-2">Edit</td>
+                  </tr>
+                  <tr>
+                    <td className="w-1/4 px-4 py-2">Scooters</td>
+                    <td className="w-1/4 px-4 py-2">45612</td>
+                    <td className="w-1/4 px-4 py-2">15</td>
+                    <td className="w-1/4 px-4 py-2">Edit</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="flex w-full flex-col gap-6   p-4">
+            <div className="gap-4 flex flex-col border rounded-lg p-4">
+              <div>Add Equipments</div>
+              <div className="flex gap-2">
+                <input
+                  className="border px-4 py-2 rounded-lg w-full"
+                  placeholder="Equipments"
+                />
+                <input
+                  className="border px-4 py-2 rounded-lg w-full"
+                  placeholder="Quantity "
+                />
+              </div>
+              <div className="flex gap-2 bgw">
+                <button className="px-4 py-2  border rounded-lg hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s">
+                  Add Item
+                </button>
+                <button className="px-4 py-2  border rounded-lg hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s">
+                  Reset
+                </button>
+              </div>
+              <div></div>
+            </div>
+            <div className="gap-4 flex flex-col border rounded-lg p-4">
+              <h3>Equipment Bookings</h3>
+              <div className="flex  border w-fit rounded-lg">
+                <div className="bg-[#026937] text-white px-4 py-2">
+                  All Bookings
+                </div>
+                <div className="  px-4 py-2 hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s ">
+                  Pending Bookings
+                </div>
+              </div>
+              <table className="table table-xs w-full">
+                <thead className="bg-gray-200 rounded-lg">
+                  <tr>
+                    <th className="w-1/4 px-2 py-1 text-left">
+                      Equipment Name
+                    </th>
+                    <th className="w-1/4 px-4 py-2 text-left">Equipment ID</th>
+                    <th className="w-1/4 px-4 py-2 text-left">Booked By</th>
+                    <th className="w-1/4 px-4 py-2 text-left">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="w-1/4 px-4 py-2">Bicycles</td>
+                    <td className="w-1/4 px-4 py-2">54987</td>
+                    <td className="w-1/4 px-4 py-2">Estuardo</td>
+                    <td className="w-1/4 px-4 py-2">Edit</td>
+                  </tr>
+                  <tr>
+                    <td className="w-1/4 px-4 py-2">Scooters</td>
+                    <td className="w-1/4 px-4 py-2">45612</td>
+                    <td className="w-1/4 px-4 py-2">Skylar</td>
+                    <td className="w-1/4 px-4 py-2">Edit</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
