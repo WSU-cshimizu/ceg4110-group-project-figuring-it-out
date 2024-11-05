2ec51883049438a3f688dde0442972525c5b8ac9
Shishir Paudel <sishirpaudel7@gmail.com>
2024-11-05 16:55:44 -0500

    changes on dashboard alignment

--- RCEM/rcem_nextjs/app/admin/dashboard/page.js
+++ RCEM/rcem_nextjs/app/admin/dashboard/page.js
@@ -5,16 +5,16 @@ function page() {
     <PageLayout>
       <div className="w-full flex p-8">
         <div className="flex gap-2 justify-center  w-full">
-          <div className="w-full p-2 flex flex-col gap-6">
-            <div className="overflow-y-auto h-[70vh]">
-              <div className="p-4 flex flex-col gap-2 ">
+          <div className="w-full  flex flex-col gap-6  border rounded-lg">
+            <div className="overflow-y-auto h-[70vh] ">
+              <div className="p-4 flex flex-col gap-4 ">
                 <h2 className="font-semibold text-lg">Available Equipments</h2>
-                <div className="flex gap-2">
+                <div className="flex gap-2 ">
                   <input
                     className="border px-4 py-2 rounded-lg w-full"
                     placeholder="Search Equipments "
                   />
-                  <div className="flex gap-2">
+                  <div className="flex gap-2 ">
                     <button className="px-4 py-2  border rounded-lg hover:bg-[#026937] hover:text-white hover:cursor-pointer transition-all transition-2s">
                       Search
                     </button>
@@ -24,37 +24,41 @@ function page() {
                   </div>
                 </div>
               </div>
-              <table className="table table-xs w-full">
-                <thead className="bg-gray-200 rounded-lg">
-                  <tr>
-                    <th className="w-1/4 px-4 py-2 text-left">
-                      Equipment Name
-                    </th>
-                    <th className="w-1/4 px-4 py-2 text-left">Equipment ID</th>
-                    <th className="w-1/4 px-4 py-2 text-left">Quantity</th>
-                    <th className="w-1/4 px-4 py-2 text-left">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="w-1/4 px-4 py-2">Bicycles</td>
-                    <td className="w-1/4 px-4 py-2">54987</td>
-                    <td className="w-1/4 px-4 py-2">20</td>
-                    <td className="w-1/4 px-4 py-2">Edit</td>
-                  </tr>
-                  <tr>
-                    <td className="w-1/4 px-4 py-2">Scooters</td>
-                    <td className="w-1/4 px-4 py-2">45612</td>
-                    <td className="w-1/4 px-4 py-2">15</td>
-                    <td className="w-1/4 px-4 py-2">Edit</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div className="px-4">
+                <table className="w-full ">
+                  <thead className="bg-gray-200   ">
+                    <tr className="w-full">
+                      <th className="w-1/4 px-4 py-2 text-left">
+                        Equipment Name
+                      </th>
+                      <th className="w-1/4 px-4 py-2 text-left">
+                        Equipment ID
+                      </th>
+                      <th className="w-1/4 px-4 py-2 text-left">Quantity</th>
+                      <th className="w-1/4 px-4 py-2 text-left">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="w-1/4 px-4 py-2">Bicycles</td>
+                      <td className="w-1/4 px-4 py-2">54987</td>
+                      <td className="w-1/4 px-4 py-2">20</td>
+                      <td className="w-1/4 px-4 py-2">Edit</td>
+                    </tr>
+                    <tr>
+                      <td className="w-1/4 px-4 py-2">Scooters</td>
+                      <td className="w-1/4 px-4 py-2">45612</td>
+                      <td className="w-1/4 px-4 py-2">15</td>
+                      <td className="w-1/4 px-4 py-2">Edit</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-          <div className="flex w-full flex-col gap-6   p-4">
+          <div className="flex w-full flex-col gap-6  ">
             <div className="gap-4 flex flex-col border rounded-lg p-4">
-              <div>Add Equipments</div>
+              <div className="font-semibold text-lg">Add Equipments</div>
               <div className="flex gap-2">
                 <input
                   className="border px-4 py-2 rounded-lg w-full"
@@ -76,7 +80,7 @@ function page() {
               <div></div>
             </div>
             <div className="gap-4 flex flex-col border rounded-lg p-4">
-              <h3>Equipment Bookings</h3>
+              <h3 className="font-semibold text-lg">Equipment Bookings</h3>
               <div className="flex  border w-fit rounded-lg">
                 <div className="bg-[#026937] text-white px-4 py-2">
                   All Bookings
